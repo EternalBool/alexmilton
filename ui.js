87ef261b6b7f5daf675e92c913740d798db3b6ba
EternalBool <alexanderhaley5@gmail.com>
2026-05-29 03:47:33 -0500

    certified ui

--- ui.js
+++ ui.js
@@ -6,19 +6,23 @@ const playthings = {
         opened: "20rem",
         description: `<p>BATTLE TO DEFF with Miles =) Brack =] Curly =} & Botto B)! Fight with these custom characters in a twist of the classic game. Battle Pong is a 'Pong' inspired project which is my first Unity game</p>`,
         controls:   
-        `<p>Player 1 ~
-    W - Up
-    S - Down
-    A - Decrease Bot Difficulty   [ B) ]
-    D - Increase Bot Difficulty   [ B) ]
-
-Player 2 ~
-    I - Up
-    K - Down
-    J - Decrease Bot Difficulty   [ B) ]
-    L - Increase Bot Difficulty   [ B) ]
-
-R - Quick Restart (During Game Over Panel)</p>`,
+        `<div>
+            <section>
+            <span style="display: block; text-align: center">Player 1 ~</span>
+                W - Up<br>
+                S - Down<br>
+                A - Decrease Bot Difficulty   [ B) ]<br>
+                D - Increase Bot Difficulty   [ B) ]<br>
+            </section>
+            <section>
+            <span style="display: block; text-align: center">Player 2 ~</span>
+                I - Up<br>
+                K - Down<br>
+                J - Decrease Bot Difficulty   [ B) ]<br>
+                L - Increase Bot Difficulty   [ B) ]<br>
+            </section>
+        </div>
+        <p>R - Quick Restart (During Game Over Panel)</p>`,
         skills: ["Unity", "C#"],
     },
 }
@@ -28,22 +32,18 @@ function toggleDesc(frame, button) {
     if (playthings[frame].toggled) {
         if (playthings[frame].lastTab == button){
             console.log(`${frame}: ${button} tab closed`);
-            description.style.maxHeight = playthings[frame].closed;
+            conjureContents(description, frame, false);
             frameToggle(frame, false);
         } else {
             console.log(`${frame}: ${button} tab opened`);
-            description.style.maxHeight = playthings[frame].closed;
-            setTimeout(() => {
-                description.innerHTML = playthings[frame][button];
-                description.style.maxHeight = playthings[frame].opened;
-            }, 700)
+            conjureContents(description, frame, false);
+            setTimeout(() => {conjureContents(description, frame, true, button);}, 700)
             frameToggle(frame, true, button);
         }
     } else {
         if (playthings[frame].lastTab == null) {
             console.log(`${frame}: ${button} tab opened`);
-            description.innerHTML = playthings[frame][button];
-            description.style.maxHeight = playthings[frame].opened;
+            conjureContents(description, frame, true, button);
             frameToggle(frame, true, button);
         }
     }
@@ -52,6 +52,15 @@ function frameToggle(frame, set, button) {
     playthings[frame].toggled = set;
     playthings[frame].lastTab = set ? button : null;
 }
-function conjureContents(set) {
-
+function conjureContents(description, frame, set, button) {
+    if (set) {
+        if (button == "skills") {
+            description.innerHTML = `<ul>${playthings[frame].skills.map(skill => `<li>${skill}</li>`).join("")}</ul>`    
+        } else {
+            description.innerHTML = playthings[frame][button];
+        }
+        description.style.maxHeight = playthings[frame].opened;
+    } else {
+        description.style.maxHeight = playthings[frame].closed;
+    }
 }
