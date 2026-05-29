6fdcaaa0b8cf9db07bbaebe556aae35f0af9ba5c
EternalBool <alexanderhaley5@gmail.com>
2026-05-28 23:25:16 -0500

    js modularity

--- ui.js
+++ ui.js
@@ -3,9 +3,22 @@ const playthings = {
         toggled: false,
         lastTab: null,
         closed: "0rem",
-        opened: "10rem",
-        description: "Battle Pong...",
-        controls:   "WASD...",
+        opened: "20rem",
+        description: `<p>BATTLE TO DEFF with Miles =) Brack =] Curly =} & Botto B)! Fight with these custom characters in a twist of the classic game. Battle Pong is a 'Pong' inspired project which is my first Unity game</p>`,
+        controls:   
+        `<p>Player 1 ~
+    W - Up
+    S - Down
+    A - Decrease Bot Difficulty   [ B) ]
+    D - Increase Bot Difficulty   [ B) ]
+
+Player 2 ~
+    I - Up
+    K - Down
+    J - Decrease Bot Difficulty   [ B) ]
+    L - Increase Bot Difficulty   [ B) ]
+
+R - Quick Restart (During Game Over Panel)</p>`,
         skills: ["Unity", "C#"],
     },
 }
@@ -16,25 +29,29 @@ function toggleDesc(frame, button) {
         if (playthings[frame].lastTab == button){
             console.log(`${frame}: ${button} tab closed`);
             description.style.maxHeight = playthings[frame].closed;
-            playthings[frame].toggled = false;
-            playthings[frame].lastTab = null;
+            frameToggle(frame, false);
         } else {
             console.log(`${frame}: ${button} tab opened`);
             description.style.maxHeight = playthings[frame].closed;
             setTimeout(() => {
-                description.querySelector(`#${frame}desc`).innerText = playthings[frame][button];
+                description.innerHTML = playthings[frame][button];
                 description.style.maxHeight = playthings[frame].opened;
             }, 700)
-            playthings[frame].toggled = true;
-            playthings[frame].lastTab = button;
+            frameToggle(frame, true, button);
         }
     } else {
         if (playthings[frame].lastTab == null) {
             console.log(`${frame}: ${button} tab opened`);
+            description.innerHTML = playthings[frame][button];
             description.style.maxHeight = playthings[frame].opened;
-            description.querySelector(`#${frame}desc`).innerText = playthings[frame][button];
-            playthings[frame].toggled = true;
-            playthings[frame].lastTab = button;
+            frameToggle(frame, true, button);
         }
     }
+}
+function frameToggle(frame, set, button) {
+    playthings[frame].toggled = set;
+    playthings[frame].lastTab = set ? button : null;
+}
+function conjureContents(set) {
+
 }
